8a625742f766b2b48ea46511677f1ec25d9ded3a
Büro Spreng <brunner@buerospreng.ch>
2025-08-20 16:26:52 +0200

    add kb-helper to the repo

---
+++ site/plugins/kb-helpers/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use Kirby\Cms\App as Kirby;
+
+// Register the plugin with Kirby
+Kirby::plugin('kesabr/kb-helpers', [
+    // Plugin options and extensions can be defined here
+]);
+
+// Include additional PHP files
+require_once __DIR__ . '/lib/convertFractionToClass.php';
+require_once __DIR__ . '/lib/filterAndProcess.php';
