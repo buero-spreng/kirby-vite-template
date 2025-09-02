01532a4645e24384080a844960d140f12f5c10c0
Büro Spreng <brunner@buerospreng.ch>
2025-09-02 10:13:26 +0200

    adujust css classes to naming convention

--- site/snippets/components/menu.php
+++ site/snippets/components/menu.php
@@ -1,11 +1,21 @@
+<?php 
+$menuItems = site()->children()->not('error');
+?>
+
 <nav class="menu">
+
   <ul class="menu__list">
-    <?php foreach ($site->children() as $item): ?>
+
+    <?php foreach ($menuItems as $item): ?>
+
       <li class="menu__item <?= $item->isActive() ? 'active' : '' ?>">
         <a href="<?= $item->url() ?>">
           <?= $item->title() ?>
         </a>
       </li>
+
     <?php endforeach ?>
+
   </ul>
+
 </nav>
