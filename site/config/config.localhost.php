bf78edf72e68865bf0aef94daa61294044eaaa24
Büro Spreng <brunner@buerospreng.ch>
2025-12-16 15:45:26 +0100

    readd content directory as a base, add config.php

--- site/config/config.localhost.php
+++ site/config/config.localhost.php
@@ -1,11 +1,12 @@
 <?php
 
+/* 
+* Only change altering from entries in config.php need to go here
+* 
+* Other config files are possible for specific environments 
+* => `config.domain.com.php`
+* => `config.test.domain.com.php`
+*/
 return [
     'debug'  => true,
-
-    'home' => 'home',
-
-    'panel' => [
-        'css' => '/assets/css/custom-panel.css'
-    ]
 ];
---
+++ site/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'debug'  => false,
+
+    'panel' => [
+        'css' => '/assets/css/custom-panel.css'
+    ]
+];
