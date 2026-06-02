71d7379a8222e854605f27b2241aa13c0fb30d8b
Büro Spreng <brunner@buerospreng.ch>
2026-06-02 10:45:39 +0200

    update kirby version

--- site/snippets/parts/favicons.php
+++ site/snippets/parts/favicons.php
@@ -1,18 +1,10 @@
 <?php
 $siteIcon = $site->siteIcon()->toFile();
-
-if (!$siteIcon) {
-    $favicon = '';
-    $icon = '';
-    $appleTouchIcon = '';
-} else {
-    $favicon = $siteIcon->thumb(['format' => 'ico', 'width' => 32, 'height' => 32])->url();
-    $icon = $siteIcon->thumb(['format' => 'svg'])->url();
-    $appleTouchIcon = $siteIcon->thumb(['format' => 'png', 'width' => 180, 'height' => 180])->url();
-}
+if ($siteIcon):
+    $png32  = $siteIcon->thumb(['format' => 'png', 'width' => 32,  'height' => 32]);
+    $png180 = $siteIcon->thumb(['format' => 'png', 'width' => 180, 'height' => 180]);
 ?>
-
-<link rel="icon"              href="<?= $favicon ?>" sizes="32x32" type="image/ico">
-<link rel="icon"              href="<?= $icon ?>" type="image/svg+xml">
-<link rel="apple-touch-icon"  href="<?= $appleTouchIcon ?>" sizes="180x180" type="image/png">
-<link rel="shortcut icon"     href="<?= $favicon ?>" type="image/ico">
+<link rel="icon"             href="<?= $siteIcon->url() ?>"  type="image/svg+xml">
+<link rel="icon"             href="<?= $png32->url() ?>"     sizes="32x32" type="image/png">
+<link rel="apple-touch-icon" href="<?= $png180->url() ?>"    sizes="180x180">
+<?php endif ?>
